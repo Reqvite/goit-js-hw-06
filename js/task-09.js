57f6a440fccf38c46e53fc61901fb4f1e0248b7d
Reqvite <reqvite2233@gmail.com>
2022-08-16 17:54:27 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const colorNameSpanRef = document.querySelector("span");
+const buttonRef = document.querySelector('button');
+
+
+buttonRef.addEventListener('click', () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorNameSpanRef.textContent = getRandomHexColor();
+})
